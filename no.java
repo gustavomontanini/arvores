8fac684cb58a3afcad397c9f60f568be9213337b
gustavomontanini <168143050+gustavomontanini@users.noreply.github.com>
2025-05-15 20:16:50 -0300

    firt_commit

---
+++ no.java
@@ -0,0 +1,11 @@
+public class No {
+    String valor;
+    No esquerdo;
+    No direito;
+
+    public No(String valor) {
+        this.valor = valor;
+        this.esquerdo = null;
+        this.direito = null;
+    }
+}
