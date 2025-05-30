82c115302ab5888723eb2365fd54342989e36511
gustavomontanini <168143050+gustavomontanini@users.noreply.github.com>
2025-05-29 21:11:48 -0300

    Update No.java
    Inclusão do atributo "Fator de balanceamento" para árvore AVL

--- No.java
+++ No.java
@@ -8,6 +8,51 @@ class No {
     }
 }
 
+class No {
+    private String valor;
+    private No esquerda, direita;
+    private int fatorBalanceamento;
+
+    public No(String valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+        this.fatorBalanceamento = 0;
+    }
+    // Getters e Setters
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public No getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(No esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public No getDireita() {
+        return direita;
+    }
+
+    public void setDireita(No direita) {
+        this.direita = direita;
+    }
+
+    public int getFatorBalanceamento() {
+        return fatorBalanceamento;
+    }
+
+    public void setFatorBalanceamento(int fatorBalanceamento) {
+        this.fatorBalanceamento = fatorBalanceamento;
+    }
+}
+
 public class Arvore {
     No raiz;
 }
